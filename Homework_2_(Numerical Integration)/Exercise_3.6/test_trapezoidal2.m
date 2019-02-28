0f59283a94daaace7f414d163a1abf3ea2910791
Suraj Jaiswal <35739373+SurajJaisz@users.noreply.github.com>
2019-02-28 16:12:20 +0200

    Work in progress

--- Homework_2_(Numerical Integration)/Exercise_3.6/test_trapezoidal2.m
+++ Homework_2_(Numerical Integration)/Exercise_3.6/test_trapezoidal2.m
@@ -1,11 +1,21 @@
-% % Exercise 3.6: Explore rounding errors with large numbers
-% % The trapezoidal method integrates linear functions exactly, and this property
-% % was used in the test function test_trapezoidal_linear in the file test_
-% % trapezoidal.m. Change the function used in Sect. 3.4.2 to f.x/ D 6108x4106
-% % and rerun the test. What happens? How must you change the test to make it useful?
-% % How does the convergence rate test behave? Any need for adjustment?
-% % Filename: test_trapezoidal2.m.
+% Created by:   Suraj Jaiswal
+% Date:         27.02.2019
+% Course:       BK70A0600_07.01.2019 Computational Methods in Mechanics
+% Exercise:     Exercise 3.6: Explore rounding errors with large numbers
 
-clc; clear; close all;
+%% test_trapezoidal2
+% Check that linear functions are integrated exactly
+f = @(x) 6*10^8*x - 4*10^6;
+F = @(x) 3*10^8*x^2 - 4*10^6*x; % Anti-derivative
+a = 1.2; b = 4.4;
+exact = F(b) - F(a); % expected result
+tol = 1E-14;
+for n =  1:4
+    numerical = trapezoidal(f, a, b, n); % computed result
+    error = abs(exact - numerical);
+    assert(error < tol, 'n=%d, err=%g', n, error);
+end
+
+
 
 
